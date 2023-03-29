c15f0b3fd6c65f67dd69e4ad46e384005dd11fd5
caonhathao <caonhathao2372004@gmail.com>
2023-03-29 13:53:50 +0700

    add new library: gobal variables

---
+++ spaceshipDefend/variable.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _VARIABLE_H
+
+#define _VARIABLE_H
+#include<iostream>
+#include"struct.cpp"
+using namespace std;
+
+extern int g_printSpeed;
+
+extern int task_point;
+extern int g_count_down;
+extern int task_completed;
+extern int *choice;
+
+extern char c;
+
+extern Position cursorPos;
+//extern vector<infoPlayer>Data = {};
+#endif // !_VARIABLE_H
